01578e6653cbaf3f3c502c1a3a0c28cfa13ef4d8
unknown <huynhchitrung020503@gmail.com>
2024-02-26 21:27:33 +0700

    Add env

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,18 @@ import { router as authRouter } from "./modules/auth/auth.controller";
 
 const app = new Hono().basePath("/api");
 
+app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://gym-master-server.vercel.app",
+    ],
+    credentials: true,
+  }),
+);
+
 app.route("/", authRouter);
 
 app.post("/", async (c) => {
@@ -14,3 +26,11 @@ app.post("/", async (c) => {
 serve(app, () => {
   console.log("Server is running on http://localhost:3000");
 });
+function cors(arg0: { origin: string[]; credentials: boolean; }): import("hono").MiddlewareHandler<import("hono").Env, "/api/*", {}> {
+  throw new Error("Function not implemented.");
+}
+
+function logger(): import("hono").MiddlewareHandler<import("hono").Env, "/api/*", {}> {
+  throw new Error("Function not implemented.");
+}
+
